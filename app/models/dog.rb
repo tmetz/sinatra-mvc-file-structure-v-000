eb9041e3de7a7d08e481a37202746b9ae06a5d8f
Tammy Metz <tammy@tammymetz.com>
2018-11-04 20:37:34 +0000

    automatically backed up by learn

---
+++ app/models/dog.rb
@@ -0,0 +1,12 @@
+class Dog
+  attr_accessor :name, :breed, :age
+
+  @@all = []
+
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+    @@all << self
+  end
+end
